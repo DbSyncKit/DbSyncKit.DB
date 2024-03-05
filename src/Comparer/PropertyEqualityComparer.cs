53ffcf21d315d185ee6c5872c26d20e9470949a2
RohitM-IN <rohit.mahajan.in@gmail.com>
2024-03-06 01:07:00 +0530

    Moved Fetcher to DB from core as its fetching from DB

---
+++ src/Comparer/PropertyEqualityComparer.cs
@@ -0,0 +1,58 @@
+﻿using System.Reflection;
+
+namespace DbSyncKit.DB.Comparer
+{
+    /// <summary>
+    /// Compares instances of data contracts based on specified properties, which can be either key or comparable properties.
+    /// </summary>
+    public class PropertyEqualityComparer<T> : IEqualityComparer<T>
+    {
+        /// <summary>
+        /// Gets the array of <see cref="PropertyInfo"/> objects representing properties used for equality comparison.
+        /// These properties can serve as either key or comparable properties.
+        /// </summary>
+        public readonly PropertyInfo[] properties;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PropertyEqualityComparer{T}"/> class.
+        /// </summary>
+        /// <param name="Properties">An array of <see cref="PropertyInfo"/> objects representing properties used for equality comparison. These can be either key or comparable properties.</param>
+        public PropertyEqualityComparer(PropertyInfo[] Properties)
+        {
+            properties = Properties;
+        }
+
+        /// <summary>
+        /// Determines whether two instances of the data contract are equal based on the specified properties.
+        /// </summary>
+        /// <param name="x">The first instance to compare.</param>
+        /// <param name="y">The second instance to compare.</param>
+        /// <returns><c>true</c> if the instances are equal; otherwise, <c>false</c>.</returns>
+        public bool Equals(T? x, T? y)
+        {
+            return properties.All(prop => Equals(prop.GetValue(x), prop.GetValue(y)));
+        }
+
+        /// <summary>
+        /// Returns a hash code for the specified instance of the data contract based on the specified properties.
+        /// </summary>
+        /// <param name="obj">The instance for which to get the hash code.</param>
+        /// <returns>A hash code for the specified instance.</returns>
+        public int GetHashCode(T obj)
+        {
+            unchecked
+            {
+                int hash = 17;
+
+                foreach (var prop in properties)
+                {
+                    var value = prop.GetValue(obj);
+                    hash = hash ^ ((value?.GetHashCode() ?? 0) + 23);
+                }
+
+                return hash;
+            }
+        }
+    }
+
+}
